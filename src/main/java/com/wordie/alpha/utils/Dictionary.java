38e3c5d28da01d2d4b04bb2272336dd6604f16f0
Digital Nabha <digitalizeindia07@gmail.com>
2022-03-13 11:28:48 +0530

    Added classloader functionality to read from jar

--- src/main/java/com/wordie/alpha/utils/Dictionary.java
+++ src/main/java/com/wordie/alpha/utils/Dictionary.java
@@ -1,6 +1,10 @@
 package com.wordie.alpha.utils;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,10 +16,13 @@ public class Dictionary {
     private static Set<String> wordsSet;//todo check if we can initialise it as soon as server restarts
 
     public Dictionary() throws IOException {
-        Path path = Paths.get("alpha/src/main/resources/words_alpha.txt");
-        byte[] readBytes = Files.readAllBytes(path);
-        String wordListContents = new String(readBytes, "UTF-8");
-        String[] words = wordListContents.split("\r\n");
+        ClassLoader cl = this.getClass().getClassLoader();
+        InputStream inputStream = cl.getResourceAsStream("words_alpha.txt");
+//        Path path = Paths.get("alpha/src/main/resources/words_alpha.txt");
+//        byte[] readBytes = Files.readAllBytes(path);
+        String contents = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+//        String wordListContents = new String(contents);
+        String[] words = contents.split("\r\n");
         wordsSet = new HashSet<>();
         Collections.addAll(wordsSet, words);
     }
