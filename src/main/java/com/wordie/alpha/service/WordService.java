e0da0b7c20f047b975d9c022ea717c4ffc523c48
Digital Nabha <digitalizeindia07@gmail.com>
2022-03-13 09:43:58 +0530

    added sql and spring logging

--- src/main/java/com/wordie/alpha/service/WordService.java
+++ src/main/java/com/wordie/alpha/service/WordService.java
@@ -4,6 +4,8 @@ import com.wordie.alpha.constants.MessageConstants;
 import com.wordie.alpha.entity.WordsEntryEntity;
 import com.wordie.alpha.repo.WordsRepo;
 import com.wordie.alpha.request.AddWordRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,15 @@ public class WordService {
     @Autowired
     private WordsRepo wordsRepo;
 
+    Logger logger = LoggerFactory.getLogger(WordService.class);
+
     public ResponseEntity<String> addWord(AddWordRequest request) {
         String word = request.getWord();
         LocalDate date = LocalDate.parse(request.getDate());
         WordsEntryEntity newWordEntry = new WordsEntryEntity(word, date);
-        wordsRepo.save(newWordEntry);
+        WordsEntryEntity savedWord = wordsRepo.save(newWordEntry);
+        logger.info("New word " + newWordEntry.getTargetWord() + " new word date " + newWordEntry.getDate()
+                + " and saved word " + savedWord.getTargetWord() + " saved word date " + savedWord.getDate());
         return ResponseEntity.status(HttpStatus.OK).body(MessageConstants.ADD_WORD_SUCCESS_MESSAGE);
     }
 }
